68864c2a5f893d0ed310ffe78ebef479dd92ce7f
hmkim829 <hmkim829@gmail.com>
2022-06-20 00:31:11 +0900

    Initial commit

---
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,30 @@
+package chap02;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordStrengthMeterTest {
+
+    @Test
+    void name(){
+
+    }
+
+    @Test
+    void meetsAllCriteria_then_strong(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG, result);
+        PasswordStrength result2 = meter.meter("abc1!Add");
+        assertEquals(PasswordStrength.STRONG, result2);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@A");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
+}
